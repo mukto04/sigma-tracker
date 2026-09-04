931aa725493fa1408b90b508ed9bb4c3c5d81538
mukto04 <muktoarifin@gmail.com>
2026-09-04 11:57:58 +0600

    Re-add Edge runtime globally to pass next-on-pages validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 
 const inter = Inter({ subsets: ["latin"] });
